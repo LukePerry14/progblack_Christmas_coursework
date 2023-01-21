e43183d045b612e7b98c1b81b01afe70453a4910
LukePerry14 <srhp63@durham.ac.uk>
2023-01-21 16:33:59 +0000

    functionality to load items into list

--- indexScript.js
+++ indexScript.js
@@ -33,3 +33,24 @@ app.get('/TodayToDo', function(req, resp){
   
   resp.send(200)
 })
+
+
+function loadjson(subsec){
+  fetch('./ToDos.json')
+      .then(res =>{
+          return res.json();
+      })
+      .then(data => {
+          document.getElementById('list').innerHTML = "";
+          var sec = data[subsec]
+          var sub = Object.keys(sec)
+
+          sub.forEach(item => {
+              console.log(item)
+              var tit = sec[item]
+              var n_item = '<li id = ' + 'no' + item + '>' + tit.title + '</li>';
+              document.getElementById('list').insertAdjacentHTML('beforeend', n_item);
+          });
+      })
+      .catch(error => console.log(error));
+}
